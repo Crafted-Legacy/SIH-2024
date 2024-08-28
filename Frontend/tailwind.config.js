3e0d63d061e3ca441da71b8d083f9460ae3bbdd2
BenakaParashuram <benakap3@gmail.com>
2024-08-28 16:56:19 +0530

    added nav

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -2,8 +2,80 @@
 module.exports = {
   content: ["./public/*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a", "950": "#172554" }
+      }
+    },
+    colors: {
+      'text': '#080c0b',
+      'background': '#f6f9f9',
+      'primary': '#67a295',
+      'secondary': '#a8a6c9',
+      'accent': '#a08db9',
+    },
+
+    fontSize: {
+      sm: '0.750rem',
+      base: '1rem',
+      xl: '1.333rem',
+      '2xl': '1.777rem',
+      '3xl': '2.369rem',
+      '4xl': '3.158rem',
+      '5xl': '4.210rem',
+    },
+    
+    fontFamily: {
+      heading: 'Noto Sans Mandaic',
+      body: 'Noto Sans Mandaic',
+    },
+    fontWeight: {
+      normal: '400',
+      bold: '700',
+    },
+
+    fontFamily: {
+      'body': [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ],
+      'sans': [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ]
+    }
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
+  content: [
+    "./node_modules/flowbite/**/*.js"
+  ],
 }
 
